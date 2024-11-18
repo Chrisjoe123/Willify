f0a39a5c54da258225d58a31639551feb8fabb1c
Christian <136973429+Chrisjoe123@users.noreply.github.com>
2024-11-19 02:30:36 +0700

    Delete JavaScript directory

--- JavaScript/SongsPage.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    var listenNowButton = document.getElementById('listenNowButton');
-    if (listenNowButton) {
-        listenNowButton.addEventListener('click', function() {
-            window.location.href = "/HTML/SongsDetailPageList/SongsDetailPage_Espresso.html";
-        });
-    }
-});
